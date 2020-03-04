e3a494436ce90b4aff6c60bff091cc5d2655570d
Lukas Netz <netz@cvc.digital>
2020-03-04 17:41:38 +0100

    fixed missing codestyle

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -1,12 +1,28 @@
 <?php
+
 declare(strict_types=1);
+
+/*
+ * WebAuthn extension for TYPO3 CMS
+ * Copyright (C) 2020 CARL von CHIARI GmbH
+ *
+ * This file is part of the TYPO3 CMS project.
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 3
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
 return [
     \Cvc\Typo3\CvcWebauthn\Domain\Model\Key::class => [
         'tableName' => 'tx_cvcwebauthn_keys',
         'properties' => [
             'crdate' => [
-                'fieldname' => 'crdate'
+                'fieldname' => 'crdate',
             ],
         ],
     ],
