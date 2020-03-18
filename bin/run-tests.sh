2c3cb6dd58eec22170c4de49ac48f2e359fdd653
Marc Lindemann <lindemann@cvc.digital>
2020-03-18 08:42:55 +0100

    integrate ddev for testing

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-
-export typo3DatabaseDriver="pdo_sqlite"
-export typo3DatabaseName="typo3"
-php .Build/bin/phpunit -c ./phpunit.xml
+ddev exec php .Build/bin/phpunit -c ./phpunit.xml
